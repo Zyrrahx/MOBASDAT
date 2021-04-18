894dbc44aa7c532979ec44109f19265dbdd13e68
Zyrrahx <Advent@hotmail.com.au>
2021-04-18 19:13:33 +1000

    Begginings of Lua testing and application

---
+++ Linux Workstation/Playable_Actor/LuaLooseFiles/readTest.lua
@@ -0,0 +1,6 @@
+--[[comment Testing
+This LUA file is to be used to test and learn the capabilities of lua and how easy it is to read and hook data from LUA into C++
+--]]
+
+local blankVariableTest = 256;                  --Pretty sure this assumes that variables are automatically assigned their datatype
+
