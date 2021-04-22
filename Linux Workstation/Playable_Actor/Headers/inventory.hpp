2ca724b5ddc4ffbf444191c39e6503d515662940
Zyrrahx <Advent@hotmail.com.au>
2021-04-22 23:34:35 +1000

    help

---
+++ Linux Workstation/Playable_Actor/Headers/inventory.hpp
@@ -0,0 +1,24 @@
+#ifndef INVENTORY_HPP
+#define INVENTORY_HPP
+#include "abilityList.hpp"
+
+namespace LINUX {
+    class Inventory {
+        Item items[6];
+    };
+
+    class Item {
+
+    };
+}
+
+namespace ItemPool {
+
+
+
+
+}
+
+
+
+#endif
