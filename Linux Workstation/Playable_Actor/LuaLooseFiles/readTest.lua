1dc331271aa5bb50abaac609bf5eea8fa45c5abc
Zyrrahx <Zyrrahx@hotmail.com.au>
2021-04-28 01:00:18 +1000

    Testing Commit on Windows

--- Linux Workstation/Playable_Actor/LuaLooseFiles/readTest.lua
+++ Linux Workstation/Playable_Actor/LuaLooseFiles/readTest.lua
@@ -1,6 +1,6 @@
 --[[comment Testing
 This Lua file is to be used to test and learn the capabilities of lua and how easy it is to read and hook data from Lua into C++
 --]]
-
+    --Testing changes in Windows to push to github--
 local blankVariableTest = 256;                  --Pretty sure this assumes that variables are automatically assigned their datatype
 
